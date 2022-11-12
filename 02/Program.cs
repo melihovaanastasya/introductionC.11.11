b0f9c9672b775f2b8de444ea17f74cb566fbbd2f
Anastasiya <melihovaanastasya@yandex.ru>
2022-11-13 00:05:41 +0300

    02

---
+++ 02/Program.cs
@@ -0,0 +1,8 @@
+﻿// С клавиатуры вводятся два вещественных числа. Проверять является ли одно из них квадратом второго (блок-схема)
+
+System.Console.WriteLine("Введите число a:");
+double a=Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите число b:");
+double b=Convert.ToDouble(Console.ReadLine());
+if (a*a==b) System.Console.WriteLine($"{b} является квадратом {a}");
+if (b*b==a) System.Console.WriteLine($"{a} является квадратом {b}");
