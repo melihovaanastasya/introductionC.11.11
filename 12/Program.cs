b5ef9148ddf70fe37bb8c7c48d641bc89ace0b2f
Anastasiya <melihovaanastasya@yandex.ru>
2022-11-13 12:18:59 +0300

    12

---
+++ 12/Program.cs
@@ -0,0 +1,8 @@
+﻿// С клавиатуры вводится целое число из  диапазона  [10, 99]. Показать наибольшую цифру числа
+
+int a=Convert.ToInt32 (Console.ReadLine());
+int b,c;
+b=a%10;
+c=a/10;
+if (b>c) System.Console.WriteLine(b);
+else System.Console.WriteLine(c);
